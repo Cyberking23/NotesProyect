54b07420969e3d22c1688501938e48abc2a58239
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-10-23 09:01:46 -0600

    Add notes
    Add notes

--- FrontEnd/src/Pages/Home/Home.jsx
+++ FrontEnd/src/Pages/Home/Home.jsx
@@ -1,7 +1,15 @@
 import NoteCard from "../../Components/Cards/NoteCard";
 import Navbar from "../../Components/NavBar/NavBar";
-import {MdAdd} from "react-icons/md"
+import { MdAdd } from "react-icons/md";
+import AddEditNotes from "./AddEditNotes";
+import { useState } from "react";
+import Modal from "react-modal";
 function Home() {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
   return (
     <>
       <Navbar />
@@ -20,9 +28,28 @@ function Home() {
           />
         </div>
       </div>
-      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" onClick={()=>{}}>
-        <MdAdd className="text-[32px] text-white"/>
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {
+          setOpenAddEditModal({isShown:true,type:"add",data:null})
+        }}
+      >
+        <MdAdd className="text-[32px] text-white" />
       </button>
+
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgrounColor: "rgba(0,0,0,0.2)",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 " //overflow-scrooll
+      >
+        <AddEditNotes />
+      </Modal>
     </>
   );
 }
