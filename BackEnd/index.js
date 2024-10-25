20b5ec27b543fdf9e5932802730d0f3de69365d6
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-10-25 10:36:06 -0600

    Borrado de api add-note revision
    Borrado de api add-note revision

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -113,40 +113,7 @@ app.post("/login", async(req,resp)=>{
     }
 })
 
-app.post("/add-note", authenticateToken , async(req,resp)=>{
-    const {title,content,tags}=req.body
-    const {user}=req.user
-    
-    if(!title){
-        return resp.status(400).json({error:true,message:"Title is required"})
-    }
-    if(!content){
-        return resp 
-        .status(400)
-        .json({error:true,message:"Content is required"})
-    }
 
-    try{
-        const note = new Note({
-            title,content,
-            tags:tags || [],
-            userId: user._id
-        })
-
-        await note.save()
-
-        return resp.json({
-        error:false,
-        note,
-        message: "Note added succesfully",
-        })
-    } catch(error){
-        return resp.status(500).json({
-            error:true,
-            message:"Internal server error"
-        })
-    }
-})
 
 
 
